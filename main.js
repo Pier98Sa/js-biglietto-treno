749e4cfd5f4ff5f933db4e22cac2b0028fc1cf14
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-01-20 15:51:34 +0100

    aggiunto il tag p nell'html per stampare i due messaggi separatamente

--- main.js
+++ main.js
@@ -8,19 +8,22 @@ let km = parseInt(prompt("Inserisci il numero di chilometri del viaggio"))
 // struttura della sezione di condizione
 if (eta <= 17) {
     // calcolo costo del biglietto
-
     let prezzoBiglietto = km * prezzoKm * sconto20;
-    document.getElementById('prezzo').innerHTML = "hai diritto ad uno sconto del 20% \r il prezzo del biglietto è: " + (prezzoBiglietto.toFixed(2)) + "\u20AC";
-
-
+    //stampo il risultato
+    document.getElementById('sconto').innerHTML = "hai diritto ad uno sconto del 20%"
+    document.getElementById('prezzo').innerHTML = "il prezzo del biglietto è: " + (prezzoBiglietto.toFixed(2)) + "\u20AC";
 }else if (eta >= 65){
-
+    // calcolo costo del biglietto
     let prezzoBiglietto = km * prezzoKm * sconto40;
-    document.getElementById('prezzo').innerHTML = "hai diritto ad uno sconto del 40% \r il prezzo del biglietto è: " + (prezzoBiglietto.toFixed(2)) + "\u20AC";
+    //stampo il risultato
+    document.getElementById('sconto').innerHTML = "hai diritto ad uno sconto del 40%"
+    document.getElementById('prezzo').innerHTML = "il prezzo del biglietto è: " + (prezzoBiglietto.toFixed(2)) + "\u20AC";
 
 
 }else{
+    // calcolo costo del biglietto
     let prezzoBiglietto = km * prezzoKm ;
+    //stampo il risultato
     document.getElementById('prezzo').innerHTML = "Il prezzo del biglietto è: " + (prezzoBiglietto.toFixed(2)) + "\u20AC";
 
 }
